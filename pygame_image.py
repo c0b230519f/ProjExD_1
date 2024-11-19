9201ecdee4508a1da95c9e25781d47bbc859549b
kousuke <c0b230519f@edu.teu.ac.jp>
2024-11-19 16:55:46 +0900

    練習問題５

--- pygame_image.py
+++ pygame_image.py
@@ -10,17 +10,29 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
+    bg_img2 = pg.transform.flip(bg_x_img, True, False)
     kk_img = pg.image.load("fig/3.png")
     kk_img = pg.transform.flip(kk_img, True, False)
+    kk_img = pg.transform.rotozoom(kk_img, 10, 1.0)
     tmr = 0
+
+    bg_x = 0
+    bg_width = bg_img.get_width()
+    kk_rect = kk_img.get_rect()
+    kk_rect.center = (300, 200)
+
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-
-        screen.blit(bg_img, [0, 0]) #スクリーンサーフェイスに背景画像を貼る
+        key_lst = pg.key.get_pressed()
+        #print(key_lst[end=])
+        x=tmr
+        screen.blit(bg_img, [-(tmr%1600), 0]) #スクリーンサーフェイスに背景画像を貼る
+        screen.blit(bg_img2, [-((tmr+1600)%1600), 0])
+        screen.blit(kk_img, [300, 200])
         pg.display.update()
         tmr += 1        
-        clock.tick(10)
+        clock.tick(200)
 
 
 if __name__ == "__main__":
