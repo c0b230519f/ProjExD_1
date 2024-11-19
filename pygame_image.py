826acbadad47196304fb18ff5816aa1fa36803e3
kousuke <c0b230519f@edu.teu.ac.jp>
2024-11-19 17:50:57 +0900

    演習課題3

--- pygame_image.py
+++ pygame_image.py
@@ -4,18 +4,15 @@ import pygame as pg
 
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
-
 def main():
     pg.display.set_caption("はばたけ！こうかとん")
     screen = pg.display.set_mode((800, 600))
-    clock  = pg.time.Clock()
+    clock = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
-    bg_img2 = pg.transform.flip(bg_x_img, True, False)
+    bg_img2 = pg.transform.flip(bg_img, True, False)
     kk_img = pg.image.load("fig/3.png")
     kk_img = pg.transform.flip(kk_img, True, False)
     kk_img = pg.transform.rotozoom(kk_img, 10, 1.0)
-    tmr = 0
-    a = 3
     bg_x = 0
     bg_width = bg_img.get_width()
     kk_rect = kk_img.get_rect()
@@ -24,15 +21,28 @@ def main():
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-        key_lst = pg.key.get_pressed()
-        #print(key_lst[end=])
-        screen.blit(bg_img, [-(tmr%1600), 0]) #スクリーンサーフェイスに背景画像を貼る
-        screen.blit(bg_img2, [-((tmr+1600)%1600), 0])
-        screen.blit(kk_img, [300, 200])
-        pg.display.update()
-        tmr += 1        
-        clock.tick(200)
 
+        bg_x -= 2
+        if bg_x <= -bg_width:
+            bg_x += bg_width
+
+        screen.blit(bg_img, [bg_x, 0])
+        screen.blit(bg_img2, [bg_x + bg_width, 0])
+        screen.blit(bg_img, [bg_x - bg_width, 0])
+        screen.blit(bg_img2, [bg_x, 0])
+        keys = pg.key.get_pressed()
+        dx, dy = -2, 0
+        if keys[pg.K_UP]:
+            dy -= 1
+        if keys[pg.K_DOWN]:
+            dy += 1
+        if keys[pg.K_RIGHT]:
+            dx += 4
+
+        kk_rect.move_ip(dx, dy)
+        screen.blit(kk_img, kk_rect)
+        pg.display.update()
+        clock.tick(200)
 
 if __name__ == "__main__":
     pg.init()
